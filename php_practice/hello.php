36e9fcb0a4f93c1886953e7761c994fcb46cc8f2
TristanDincau <142362731+TristanDincau@users.noreply.github.com>
2025-01-10 00:28:53 +0000

    end of 1/9

---
+++ php_practice/hello.php
@@ -0,0 +1,8 @@
+<?php
+
+$myFriend = "Lily";
+$myName = "Tristan";
+echo "Hello, World! \n";
+echo "My name is $myName. \n";
+echo "My friends name is $myFriend.", "and Jensen";
+echo 'Hello $myFriend \n';
